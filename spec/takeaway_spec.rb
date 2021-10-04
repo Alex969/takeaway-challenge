67a43ef84d721b8cf4ada90b90541c9806c66765
Stewart-Alexander ANNANI <wxst.stewart@gmail.com>
2021-10-04 13:44:19 +0200

    fixed report_menu calling issue

--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -3,7 +3,7 @@ require 'takeaway'
 describe Takeaway do
   subject(:takeaway) { described_class.new(menu: menu) }
 
-  let(:menu) { double(:menu, print: printed_menu) }
+  let(:menu) { double(:menu, return_menu: printed_menu) }
   let(:printed_menu) { "Fire noodles: £10.50" }
 
   it 'shows the menu' do
